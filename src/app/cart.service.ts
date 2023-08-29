83360a5d505b27ae1dd2f9008ab70595eca102d9
ayoungmk <54252392+ayoungmk@users.noreply.github.com>
2023-08-29 11:24:00 +0100

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -23,8 +23,9 @@ export class CartService {
     return this.items;
   }
 
-  getShippingPrices(){
-    return this.http.get<{type: string, price: number}[]>
-    ('/assets/shipping.json');
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
